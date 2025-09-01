a3b2b32f180d144ad3ac00e9d183dd64ee4d3504
Zammil00 <muzammilkw07@gmail.com>
2025-09-01 22:37:25 +0700

    Fix Todo Repository

--- lib/features/todo/data/repositories/todo_repository_impl.dart
+++ lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -1,13 +1,64 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:labil_todo/core/exceptions/server_exceptions.dart';
+import 'package:labil_todo/features/todo/data/data_sources/todo_remote_data_source.dart';
+import 'package:labil_todo/features/todo/domain/entities/todo.dart';
 import '../../domain/repositories/todo_repository.dart';
 
 class TodoRepositoryImpl implements TodoRepository {
-  const TodoRepositoryImpl();
+  final TodoRemoteDataSource _remoteDataSource;
 
-  Future<T> _run<T>(Future<T> Function() function) async {
+  TodoRepositoryImpl({required TodoRemoteDataSource remoteDataSource})
+    : _remoteDataSource = remoteDataSource;
+
+  @override
+  Future<Either<ServerExceptions, bool>> addTodo({
+    required String title,
+  }) async {
+    return run(() async {
+      await _remoteDataSource.addTodo(title: title);
+      return right(true);
+    });
+  }
+
+  @override
+  Future<Either<ServerExceptions, bool>> deleteTodo({required int id}) async {
+    return run(() async {
+      await _remoteDataSource.deleteTodo(id: id);
+      return right(true);
+    });
+  }
+
+  @override
+  Future<Either<ServerExceptions, List<Todo>>> getTodos() {
+    return run(() async {
+      final todos = await _remoteDataSource.getTodos();
+      return right(todos);
+    });
+  }
+
+  @override
+  Future<Either<ServerExceptions, bool>> updateTodo({
+    required int id,
+    String? title,
+    bool? isChecked,
+  }) {
+    return run(() async {
+      await _remoteDataSource.updateTodo(
+        id: id,
+        title: title,
+        isChecked: isChecked,
+      );
+      return right(true);
+    });
+  }
+
+  Future<Either<ServerExceptions, T>> run<T>(
+    Future<Either<ServerExceptions, T>> Function() function,
+  ) async {
     try {
       return await function();
-    } catch (e) {
-      throw e;
+    } on ServerExceptions catch (error) {
+      return left(error);
     }
   }
 }
--- lib/features/todo/domain/repositories/todo_repository.dart
+++ lib/features/todo/domain/repositories/todo_repository.dart
@@ -1,1 +1,11 @@
-abstract interface class TodoRepository {}
+import 'package:fpdart/fpdart.dart';
+import 'package:labil_todo/core/exceptions/server_exceptions.dart';
+import 'package:labil_todo/features/todo/data/models/todo_model.dart';
+import 'package:labil_todo/features/todo/domain/entities/todo.dart';
+
+abstract interface class TodoRepository {
+  Future<Either<ServerExceptions, bool>> addTodo({required String title});
+  Future<Either<ServerExceptions, bool>> deleteTodo({required int id});
+  Future<Either<ServerExceptions, List<Todo>>> getTodos();
+  Future<Either<ServerExceptions, bool>> updateTodo({required int id, String? title, bool? isChecked});
+}
