98e63f21c618e45909ee6cdff2b087d48e828009
Zammil00 <muzammilkw07@gmail.com>
2025-09-01 22:22:44 +0700

    Fix Data Source

--- lib/features/todo/data/data_sources/todo_remote_data_source_impl.dart
+++ lib/features/todo/data/data_sources/todo_remote_data_source_impl.dart
@@ -1,3 +1,4 @@
+import 'package:labil_todo/core/exceptions/server_exceptions.dart';
 import 'package:labil_todo/features/todo/data/models/todo_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -11,33 +12,52 @@ class TodoRemoteDataSourceImpl implements TodoRemoteDataSource {
 
   @override
   Future<void> addTodo({required String title}) async {
-    return _supabaseClient.from('todos').insert({'title': title});
+    return run(() {
+      return _supabaseClient.from('todos').insert({'title': title});
+    });
   }
 
   @override
   Future<void> deleteTodo({required int id}) async {
-    return _supabaseClient.from('todos').delete().eq('id', id);
+    return run(() {
+      return _supabaseClient.from('todos').delete().eq('id', id);
+    });
   }
 
   @override
   Future<List<TodoModel>> getTodos() async {
-    final response = await _supabaseClient
-        .from('todos')
-        .select()
-        .order('is_checked', ascending: true)
-        .order('title', ascending: true);
+    return run(() async {
+      final response = await _supabaseClient
+          .from('todos')
+          .select()
+          .order('is_checked', ascending: true)
+          .order('title', ascending: true);
 
-    return TodoModel.fromJsonList(response);
+      return TodoModel.fromJsonList(response);
+    });
   }
 
   @override
   Future<void> updateTodo({required int id, String? title, bool? isChecked}) {
-    return _supabaseClient
-        .from('todos')
-        .update({
-          if (title != null) 'title': title,
-          if (isChecked != null) 'is_checked': isChecked,
-        })
-        .eq('id', id);
+    return run(() {
+      final data = <String, dynamic>{};
+      if (title != null) {
+        data['title'] = title;
+      }
+      if (isChecked != null) {
+        data['is_checked'] = isChecked;
+      }
+      return _supabaseClient.from('todos').update(data).eq('id', id);
+    });
+  }
+
+  Future<T> run<T>(Future<T> Function() function) async {
+    try {
+      return await function();
+    } on PostgrestException catch (error) {
+      throw ServerExceptions(message: error.message);
+    } catch (error) {
+      throw ServerExceptions(message: error.toString());
+    }
   }
 }
